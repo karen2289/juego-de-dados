cfd0f70dd12e81eba29f682b8c41b89a0324a110
Karen Aguirre <latingirl_2289@hotmail.com>
2021-08-03 12:47:55 +0200

    Juego de dados en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  //Declaramos dos variables (aqui estaremos usando el property binding)
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice2.png';
+
+  num1 = 1;
+  num2 = 2;
+
+  //Creamos una funcion
+  tirarDados(): void {
+    this.num1 = Math.round(Math.random() * 5) + 1;
+    this.num2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.num1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.num2 + '.png';
+  }
 }
